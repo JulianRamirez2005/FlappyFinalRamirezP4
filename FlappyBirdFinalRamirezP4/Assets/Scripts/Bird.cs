30b56b1edcbdda88d3c2f74f40ea6fb5b4a0b56d
JulianRamirez2005 <solokiller2005@gmail.com>
2022-12-19 18:54:04 -0800

    Anim
    Anim

--- FlappyBirdFinalRamirezP4/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalRamirezP4/Assets/Scripts/Bird.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float upForce;                   
-    private bool isDead = false;          
+    public float upForce;                    
+    private bool isDead = false;      
 
-    private Animator anim;                   
-    private Rigidbody2D rb2d;               
+    private Animator anim;               
+    private Rigidbody2D rb2d;            
 
     void Start()
-    {   
+    {
         anim = GetComponent<Animator> ();
         rb2d = GetComponent<Rigidbody2D>();
     }
@@ -20,7 +20,7 @@ public class Bird : MonoBehaviour
     {
         if (isDead == false) 
         {
-            if (Input.GetMouseButtonDown(0)) 
+            if (Input.GetMouseButtonDown (0));
             {
                 anim.SetTrigger("Flap");
                 rb2d.velocity = Vector2.zero;
@@ -31,9 +31,7 @@ public class Bird : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger ("Die");
-        GameControl.instance.BirdDied ();
     }
 }
