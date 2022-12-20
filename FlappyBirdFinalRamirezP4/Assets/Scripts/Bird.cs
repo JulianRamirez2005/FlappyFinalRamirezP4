2c0aa26bda37a0da002dedc3491238696211f607
JulianRamirez2005 <solokiller2005@gmail.com>
2022-12-20 12:41:58 -0800

    Movin the bird
    yes

--- FlappyBirdFinalRamirezP4/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalRamirezP4/Assets/Scripts/Bird.cs
@@ -4,34 +4,45 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float upForce;                    
-    private bool isDead = false;      
+    public float upForce;
+    private bool isDead = false;
 
-    private Animator anim;               
-    private Rigidbody2D rb2d;            
+    private Animator anim;
+    private Rigidbody2D rb2d;
 
     void Start()
     {
-        anim = GetComponent<Animator> ();
+        
+        anim = GetComponent<Animator>();
+        
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (isDead == false) 
+      
+        if (isDead == false)
         {
-            if (Input.GetMouseButtonDown (0));
+   
+            if (Input.GetMouseButtonDown(0))
             {
+ 
                 anim.SetTrigger("Flap");
+
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upForce));
+
+                rb2d.AddForce(new Vector2(200f, upForce));
             }
         }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+
+        rb2d.velocity = Vector2.zero;
+
         isDead = true;
-        anim.SetTrigger ("Die");
+ 
+        anim.SetTrigger("Die");
     }
 }
